a4f4a6bc258a041830cbd7784013afd17576e2b3
Jean Lozano <michecoanexa52@gmail.com>
2023-03-28 22:02:01 -0500

    Start project

---
+++ src/main/java/com/project/coches/ProjectCochesApplication.java
@@ -0,0 +1,13 @@
+package com.project.coches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCochesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCochesApplication.class, args);
+	}
+
+}
