4373d259e7e74fb93d785b55cd2c81b5572d4b5c
Jean Lozano <michecoanexa52@gmail.com>
2023-05-03 18:15:44 -0500

    created CorsConfiguration

--- src/main/java/com/project/coches/config/WebSecurityConfig.java
+++ src/main/java/com/project/coches/config/WebSecurityConfig.java
@@ -13,6 +13,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 /**
  * Clase que configura lo relacionado a las peticiones HTTP
@@ -35,6 +42,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(withDefaults())
                 .exceptionHandling().accessDeniedHandler(accessDeniedHandlerException)
                 .and()
                 .csrf().disable()
@@ -63,6 +71,18 @@ public class WebSecurityConfig {
 
 
         return http.build();
+    }
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(CorsConfiguration.ALL));
+        configuration.setAllowedMethods(Arrays.asList(CorsConfiguration.ALL));
+        configuration.setAllowedHeaders(Arrays.asList(CorsConfiguration.ALL));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 
--- src/main/java/com/project/coches/controller/AuthController.java
+++ src/main/java/com/project/coches/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(path = "/auth")
